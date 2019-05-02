591ecad5be8022e88809b869a0f6413b9e12aafc
Brooke Kullberg and Dustin Herboldshimer <dustin@>
2019-05-02 14:32:51 -0700

    add properties to Game object, and methods and tests to start a new game

--- HangMan.Tests/ModelTests/GameTests.cs
+++ HangMan.Tests/ModelTests/GameTests.cs
@@ -6,14 +6,50 @@ using HangMan.Models;
 namespace HangMan.Tests
 {
   [TestClass]
-  public class GameTest : IDisposable
+  public class GameTest
   {
 
-    public void Dispose()
+    // public void Dispose()
+    // {
+    //     Game.ClearAll();
+    // }
+
+    [TestMethod]
+    public void GameConstructor_CreateGameInstance_Game()
     {
-        Item.ClearAll();
+      Game newGame = new Game();
+      Assert.AreEqual(typeof(Game), newGame.GetType());
     }
 
+    [TestMethod]
+    public void GetGameWord_SetWordForGame_String()
+    {
+      Game newGame = new Game();
+      string gameWord = newGame.GetGameWord();
+      bool doesContain = false;
+      List<string> _wordPool = new List<string> {"tamagotchi", "string", "bootygame", "vaporwave", "cheese", "robot", "bobby", "godzilla", "queen", "crispy", "alligator"};
+
+      for(int i = 0; i < _wordPool.Count; i ++)
+      {
+        if(gameWord == _wordPool[i])
+        {
+          doesContain = true;
+        }
+      }
+      Console.WriteLine(gameWord);
+      Assert.AreEqual(true, doesContain);
+    }
+
+    [TestMethod]
+    public void BuildWordBlanks_MakeACharArrayOfWordBlanks_CharArray()
+    {
+      Game newGame = new Game();
+      // char[] gameWordArray = newGame.GameWord;
+      // char[] testArray = {'h', 'a', 'p', 'p', 'y'};
+      char[] blanksArray = newGame.BuildWordBlanks();
+      char testChar = '_';
+      Assert.AreEqual(testChar, blanksArray[3]);
+    }
 
   }
 }
--- HangMan/Models/Game.cs
+++ HangMan/Models/Game.cs
@@ -1,8 +1,50 @@
 using System.Collections.Generic;
+using System;
+
 namespace HangMan.Models
 {
   public class Game
   {
-    
+    private List<string> _wordPool = new List<string> {"tamagotchi", "string", "bootygame", "vaporwave", "cheese", "robot", "bobby", "godzilla", "queen", "crispy", "alligator"};
+    private char[] _gameWord;
+    private char[] _wordBlank;
+    private List<char> _lettersGuessed = new List<char> {};
+    private int _numberOfGuesses = 0;
+
+    public char[] GameWord{ get => _gameWord; set => _gameWord = value; }
+    public char[] WordsBlank{ get => _wordBlank; set => _wordBlank = value; }
+    public List<char> LettersGuessed{ get; set; }
+    publi int NumberOfGuesses{ get; set; }
+
+    public Game()
+    {
+      this.MakeCharArray();
+      this.BuildWordBlanks();
+    }
+
+    public string GetGameWord()
+    {
+      Random rand = new Random();
+      // int count = _wordPool.Count;
+      // string gameWord = _wordPool[rand.Next(count-1)];
+      // return gameWord;
+      return _wordPool[rand.Next(_wordPool.Count-1)];
+    }
+
+     public void MakeCharArray()
+     {
+       string gameWordString = this.GetGameWord();
+       _gameWord = gameWordString.ToCharArray();
+     }
+
+     public char[] BuildWordBlanks()
+     {
+       _wordBlank = _gameWord;
+       for(int i = 0; i < _wordBlank.Length; i++)
+       {
+         _wordBlank[i] = '_';
+       }
+       return _wordBlank;
+     }
   }
 }
